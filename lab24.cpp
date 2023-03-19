9b99f7bd86f56ab1dbf9902ed22e3ab83e682e0d
napapon-wongkom <dot.wongkom@gmail.com>
2023-03-19 16:33:10 +0700

    lab24.cpp

--- lab24.cpp
+++ lab24.cpp
@@ -30,6 +30,71 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 
 //Write your code here
 
+ComplexNumber operator+(double number,const ComplexNumber &c){
+	return ComplexNumber(number + c.real,c.imag);
+}
+
+ComplexNumber operator-(double number,const ComplexNumber &c){
+	return ComplexNumber(number - c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(((real*c.real)-(imag * c.imag)),((real * c.imag)+(imag * c.real)));
+}
+
+ComplexNumber operator*(double number,const ComplexNumber &c){
+ 	return ComplexNumber(number*c.real,number*c.imag);
+ }
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber((((real*c.real)+(imag*c.imag))/(pow(c.real,2)+pow(c.imag,2))),((-(real*c.imag)+(c.real*imag))/(pow(c.real,2)+pow(c.imag,2))));
+}
+
+ComplexNumber operator/(double number,const ComplexNumber &c){
+ 	return ComplexNumber((((number * c.real)+(0*c.imag))/(pow(c.real,2)+pow(c.imag,2))),((-(number * c.imag)+(0*c.real))/(pow(c.real,2)+pow(c.imag,2))));
+ }
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(c.real == real && c.imag == imag){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool operator==(double number,const ComplexNumber &c){
+	if(c.real == number && c.imag == 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real) * 180/M_PI;
+}
+
+ostream & operator<<(ostream &os,const ComplexNumber &c){
+	if(c.imag!=0 && c.real != 0){
+ 		if(c.imag<0){
+ 			return os <<c.real<<c.imag<<"i";
+ 		}
+ 		else{
+ 			return os <<c.real<<"+"<<c.imag<<"i";
+ 		}
+ 	}
+ 	else if(c.imag==0 ){
+ 		return os <<c.real;
+ 	}
+ 	else {
+ 		return os <<c.imag<<"i";
+ 	}
+}
+
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
 	cout << a << "\n";
